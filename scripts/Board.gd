123b9153a00ec43dbf59a43a9cde7169475fc5c0
JoanGTSQ <49458356+JoanGTSQ@users.noreply.github.com>
2024-12-30 15:55:29 +0100

    Update Board.gd

--- scripts/Board.gd
+++ scripts/Board.gd
@@ -3,6 +3,8 @@ extends Node2D
 # Reference to the main card node
 onready var _main_card = $Card
 onready var _turn_label = $TurnLabel
+onready var _player_deck = $PlayerDeck
+onready var _say_uno_button = $SayUno
 
 # Updates the main card on the board
 func update_main_card() -> void:
@@ -21,7 +23,16 @@ func update_main_card() -> void:
 	else:
 		new_card = DeckManager.get_current_card()
 		_add_card_to_board(new_card)
+	
 
+func _enable_say_uno_button() -> void:
+	if _get_players_card_count() <= 2:
+		_say_uno_button.visible = true
+	else:
+		_say_uno_button.visible = false
+
+func _get_players_card_count() -> int:
+	return _player_deck.get_node("CardContainer").get_child_count()
 
 func _add_card_to_board(card : Card) -> void:
 	# If the new card already has a parent, remove it from there
@@ -40,6 +51,7 @@ func update_turn_label() -> void:
 		_turn_label.text = "PLAYER TURN"
 	else:
 		_turn_label.text = "ENEMY TURN"
+	_enable_say_uno_button()
 
 
 # Event triggered when the button is pressed
@@ -47,7 +59,9 @@ func _on_DrawCard_Button_pressed() -> void:
 	if GameManager.get_current_player() == GameManager.PLAYER_TURN.PLAYER:
 		GameManager.emit_signal("signal_draw_card", GameManager.PLAYER_TURN.PLAYER, false)
 
-
+func _on_say_uno_button_pressed() -> void:
+	GameManager.emit_signal("signal_say_uno", GameManager.PLAYER_TURN.PLAYER)
+	
 # Set up the main card when the board is ready
 func _on_Board_ready() -> void:
 	update_main_card()
