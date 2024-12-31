4b7d663c4616b1b9903b332a82714a6e8ef8b167
JoanGTSQ <49458356+JoanGTSQ@users.noreply.github.com>
2024-12-31 07:52:48 +0100

    Update GameManager.gd

--- scripts/GameManager.gd
+++ scripts/GameManager.gd
@@ -26,6 +26,10 @@ var _enemy_deck : Node = null
 var _enemy_deck_container : Node = null
 
 
+# Initializes the scene and connects signals.
+func _ready() -> void:
+	_connect_signals()
+
 # Gets the current player.
 # @return int The current player.
 func get_current_player() -> int:
@@ -69,6 +73,16 @@ func _apply_special_card(player : int, card : Card) -> bool:
 	return true
 
 
+# Checks if the game has ended.
+# @return bool True if the game has ended.
+func _check_game_end() -> bool:
+	for i in range(_player_hands.size()):
+		if _player_hands[i].size() == 0:
+			_end_game()
+			return true
+	return false
+
+
 # Swaps the player and enemy decks.
 func _swap_decks() -> void:
 	# Swap hands
@@ -92,11 +106,43 @@ func _swap_decks() -> void:
 	print("Decks have been swapped!")
 
 
+# Checks if it's the specified player's turn.
+# @param player The player to check.
+# @return bool True if it is the player's turn.
+func _is_player_turn(player : int) -> bool:
+	return _player_turn == player
+
+
 # Ends the game and transitions to the Game Over scene.
 func _end_game() -> void:
 	DeckManager.destroy()
 	get_tree().change_scene("res://scenes/GameOverScene.tscn")
 	print("Game ended.")
+
+
+# Executes the AI player's turn.
+func _ai_play() -> void:
+	if _player_turn == PLAYER_TURN.ENEMY:
+		for card in _player_hands[PLAYER_TURN.ENEMY]:
+			if DeckManager.is_valid_card(card):
+				randomize()
+				var r_number = randi() % 99
+				if r_number % 2:
+					emit_signal("signal_say_uno", PLAYER_TURN.ENEMY)
+				emit_signal("signal_play_card", PLAYER_TURN.ENEMY, card)
+				return
+		emit_signal("signal_draw_card", PLAYER_TURN.ENEMY, false)
+
+
+# Connects signals to their respective methods.
+func _connect_signals() -> void:
+	connect("signal_start_game", self, "_on_signal_start_game")
+	connect("signal_turn_changed", self, "_on_signal_turn_changed")
+	connect("signal_special_card_played", self, "_on_signal_special_card_played")
+	connect("signal_card_played", self, "_on_signal_card_played")
+	connect("signal_draw_card", self, "_on_signal_draw_card")
+	connect("signal_play_card", self, "_on_signal_play_card")
+	connect("signal_say_uno", self, "_on_signal_say_uno")
 
 
 # Changes the turn to the next player.
@@ -124,6 +170,16 @@ func _move_cards(player : int, card : Card) -> void:
 		_enemy_deck.reposition_cards()
 
 
+# Handles the event of the turn changing.
+func _on_signal_turn_changed() -> void:
+	var board : Node = get_tree().root.get_node("Board")
+	board.update_turn_label()
+	board.update_main_card()
+	if _player_turn == PLAYER_TURN.ENEMY:
+		yield(get_tree().create_timer(ENEMY_WAIT_TIME), "timeout")
+		_ai_play()
+
+
 # Handles the start of the game.
 func _on_signal_start_game() -> void:
 	get_tree().change_scene("res://scenes/Board.tscn")
@@ -148,3 +204,44 @@ func _on_signal_start_game() -> void:
 func _on_signal_say_uno(player : int) -> void:
 	if _player_hands[player].size() == 2:
 		_player_said_uno = true
+
+
+# Draws a card for a player.
+# @param player The player drawing the card.
+# @param special_card Whether the card is drawn due to a special effect.
+func _on_signal_draw_card(player : int, special_card : bool) -> void:
+	if player == _player_turn or special_card:
+		var card : Card = DeckManager.draw()
+		_player_hands[player].append(card)
+		_move_cards(player, card)
+	if not special_card:
+		_change_turn(false)
+
+
+# Attempts to play a card for a player.
+# @param player The player playing the card.
+# @param card The card to play.
+# @return bool True if the card was successfully played.
+func _on_signal_play_card(player : int, card : Card) -> bool:
+	if DeckManager.is_valid_card(card) and _is_player_turn(player):
+		_player_hands[player].erase(card)
+		DeckManager.discard_card(card)
+		if _check_game_end():
+			return true
+		if card.is_special_card():
+			if not _apply_special_card(player, card):
+				emit_signal("signal_special_card_played")
+				return true
+		emit_signal("signal_card_played")
+		return true
+	return false
+
+
+# Handles the event of a card being played.
+# @param card The card that was played.
+func _on_signal_card_played() -> void:
+	_change_turn(false)
+
+
+func _on_signal_special_card_played() -> void:
+	_change_turn(true)
